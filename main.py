334bbc95248d6c1568d6ece6b995e0f3f6a0c78e
mrgne1 <mrgne1@gmail.com>
2024-08-19 20:08:25 -0400

    added throttling

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
